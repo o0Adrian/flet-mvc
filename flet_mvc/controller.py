e6877f3e0044c45f86570b36ca3744133733986e
Adrian Monroy <carlos.monclef@gmail.com>
2023-06-05 00:53:28 -0500

    [0.1.0] Adding new version, code only

--- flet_mvc/controller.py
+++ flet_mvc/controller.py
@@ -2,21 +2,23 @@
 Flet Base Controller Class
 """
 
+from typing import Type
 import flet as ft
 from .alert import ERROR, WARNING, SUCCESS, ADVICE, INFO
+from .model import FletModel
 
-class FletController():
-    def __init__(self, page: ft.Page, model):
+
+class FletController:
+    def __init__(self, page: ft.Page, model: Type[FletModel]):
         self.page = page
         self.model = model
-        
+
     def update(self):
-        """ Updates the page by using self.update() instead of self.page.update() """
+        """Updates the page by using self.update() instead of self.page.update()"""
         self.page.update()
 
-        
-    def alert(self, msg="" , alert_type=ERROR):
-        """ Show Alert """
+    def alert(self, msg="", alert_type=ERROR):
+        """Show Alert"""
         if alert_type == ERROR:
             self.page.snack_bar = ft.SnackBar(
                 content=ft.Row(
@@ -29,7 +31,7 @@ class FletController():
             )
             self.page.snack_bar.open = True
             self.update()
-            
+
         elif alert_type == SUCCESS:
             self.page.snack_bar = ft.SnackBar(
                 content=ft.Row(
@@ -44,29 +46,29 @@ class FletController():
             )
             self.page.snack_bar.open = True
             self.update()
-            
+
         elif alert_type == ADVICE:
             self.page.snack_bar = ft.SnackBar(
-                    content=ft.Text(msg),
-                    action="Understood",
-                    action_color=ft.colors.BLUE_200
-                )
-            self.page.snack_bar.open = True
-            self.update()
-            
-        elif alert_type == INFO:
-            self.page.snack_bar = ft.SnackBar(
-                    content=ft.Row(
-                        controls=[
-                            ft.Icon(ft.icons.INFO_OUTLINED, color=ft.colors.WHITE),
-                            ft.Text(msg),
-                        ],
-                    ),
-                    bgcolor=ft.colors.BLUE_200
+                content=ft.Text(msg),
+                action="Understood",
+                action_color=ft.colors.BLUE_200,
             )
             self.page.snack_bar.open = True
             self.update()
-            
+
+        elif alert_type == INFO:
+            self.page.snack_bar = ft.SnackBar(
+                content=ft.Row(
+                    controls=[
+                        ft.Icon(ft.icons.INFO_OUTLINED, color=ft.colors.WHITE),
+                        ft.Text(msg),
+                    ],
+                ),
+                bgcolor=ft.colors.BLUE_200,
+            )
+            self.page.snack_bar.open = True
+            self.update()
+
         elif alert_type == WARNING:
             self.page.snack_bar = ft.SnackBar(
                 content=ft.Row(
